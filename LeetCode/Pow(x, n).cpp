9df795f74b8344ec3a98f4af7c2916907e3eece0
LQ <1053539672@qq.com>
2019-01-02 14:56:43 +0800

    Update Pow(x, n).cpp

--- LeetCode/Pow(x, n).cpp
+++ LeetCode/Pow(x, n).cpp
@@ -2,7 +2,7 @@
 //本道题有点意思，会发现int型它小于0如果最小值翻转后是为0！于是需要考虑将数字表示的形式转换为别的可以表示更大位数的形式，如unsigned、此处我增设long型去表示更大位数
 class Solution {
 public:
-    double myPow(double x, int n) {
+    double myPow(double x, int n){
         long n1 = n;
         long n2;
         if(n < 0) n2 = -n1;
